ab48a762ec758a120ff42e71a9e6e883893bead6
Roy Honders <royhonders@gmail.com>
2019-05-17 15:05:10 +0200

    add check for mockery disable when enabled

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 test $(git grep "test.only(" -- '*.js' | wc -l) -eq 0
 test $(git grep "experiment.only(" -- '*.js' | wc -l) -eq 0
 test $(git grep "suite.only(" -- '*.js' | wc -l) -eq 0
+test $(git grep "Mockery.enable(" -- '*.js' | wc -l) -eq `expr $(git grep "Mockery.disable\|Mockery.deregisterAll(" -- '*.js' | wc -l) / 2`
